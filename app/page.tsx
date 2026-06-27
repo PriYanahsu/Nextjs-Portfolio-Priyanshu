653039bef208f977f722105a643e1c41cbc9d03e
Priyanshu <priyanshu12656@gmail.com>
2026-06-27 18:26:07 +0530

    topmet

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <About />
       <Skills />
       <Experience />
-      <Services />
+      {/* <Services /> */}
       <Projects />
       <Certificate />
       <Contact />
